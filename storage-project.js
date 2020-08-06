c2e9290384433d7b1e4676e852b18f32aa3df1b2
root <root@DESKTOP-LE7491G.localdomain>
2020-08-06 12:38:30 -0700

    Did some toying around during lunch

--- storage-project.js
+++ storage-project.js
@@ -1,25 +1,30 @@
-window.addEventListener("DOMContentLoaded", (event) => {
 
+window.addEventListener("DOMContentLoaded", (event) => {
+  const largerDiv = document.getElementById("shopping-cart")
+  const itemContainer = document.createElement("ol");
   const showCart = () => {
-    
-    const shoppingCartDiv = document.createElement("div");
-    const itemContainer = document.createElement("ol");
-    
+    largerDiv.innerHTML = "<h2> Shopping Cart </h2>";
     for(let i = 0; i < localStorage.length; i++) {
+      if (i === 1) {
+        continue;
+      }
       let key = localStorage.key(i);
       let quantity = localStorage.getItem(key);
       let parseKey = JSON.parse(key);
       let parseQuantity = JSON.parse(quantity);
-      const listEl = document.createElement("li");
-      let listedItem = `${parseKey} - ${parseQuantity}`;
-      shoppingCartDiv.setAttribute("class", "shopping-cart-items");
-      listEl.innerHTML = listedItem;
-      itemContainer.appendChild(listEl);
+      let listedItem = `<li><button class="ui icon button"><i class="trash alternate outline icon"></i>Remove</button>${parseKey}: ${parseQuantity}</li>`;
+      itemContainer.innerHTML += listedItem;   
+      largerDiv.appendChild(itemContainer);
     }
-    document.body.appendChild(shoppingCartDiv);
-    shoppingCartDiv.appendChild(itemContainer);
+    localStorage.setItem("saved-cart", "true")
+    
+    
   };
-
+  
+  if (localStorage.getItem("saved-cart") !== null) {
+    showCart();
+    }
+  
   const storeItem = (e) => {
     e.preventDefault();
     const addedItem = JSON.stringify(document.getElementById("items").value);
