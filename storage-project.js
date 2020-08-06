bb182d7b1be092b52a5fd033406855da4b5dd4b8
root <root@DESKTOP-LE7491G.localdomain>
2020-08-06 09:07:33 -0700

    Finished storeItem function

--- storage-project.js
+++ storage-project.js
@@ -1,13 +1,19 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-  // const showCart = () => {
+  const showCart = () => {
+  
+  };
 
-  // };
-
-  // const storeItem = () => {
-
-  // };
-
+  const storeItem = (e) => {
+    const addedItem = JSON.stringify(document.getElementById("items").value);
+    const numberOfItems = JSON.stringify(document.getElementById("quantity").value);
+    localStorage.setItem(addedItem,numberOfItems);
+    showCart()l
+  };
+  
+  const submitButton = document.getElementById("add-to-cart")
+  submitButton.addEventListener("click", storeItem)
+  
   // const removeItem = () => {
 
   // };
